35aab6b53d95f99634da3273e517602b4aeae4f8
GC <12754600+genesaturn@users.noreply.github.com>
2021-03-24 12:55:15 +0800

    Long press to share added to links

---
+++ app.js
@@ -0,0 +1,40 @@
+const instagramLinkButton = document.getElementById('instagramLink');
+const facebookLinkButton = document.getElementById('facebookLink');
+const youtubeLinkButton = document.getElementById('youtubeLink');
+const emailButton = document.getElementById('emailAddress');
+const navigationToOfficeButton = document.getElementById('navigationToOffice');
+
+instagramLinkButton.addEventListener('contextmenu', e => {
+    e.preventDefault();
+    share("分享鐵騎社的 Instagram 帳號", "@cyut_bike", "https://www.instagram.com/cyut_bike");
+});
+
+facebookLinkButton.addEventListener('contextmenu', e => {
+    e.preventDefault();
+    share("分享鐵騎社的 Facebook 粉絲專頁", "朝陽鐵騎社", "https://www.facebook.com/cyutcyct");
+});
+
+youtubeLinkButton.addEventListener('contextmenu', e => {
+    e.preventDefault();
+    share("分享鐵騎社的 Youtube 頻道", "朝陽鐵騎社", "https://www.youtube.com/channel/UCWN-rVESPb9jmAUrKTNzqag/featured");
+});
+
+emailButton.addEventListener('contextmenu', e => {
+    e.preventDefault();
+    share("分享鐵騎社的電子郵件地址", "ccteam168@gmail.com", "");
+});
+
+navigationToOfficeButton.addEventListener('contextmenu', e => {
+    e.preventDefault();
+});
+
+
+function share(shareTitle, shareText, shareUrl){
+    if (navigator.share) {
+        navigator.share({
+          title: shareTitle,
+          text: shareText,
+          url: shareUrl,
+        })
+      }
+}
